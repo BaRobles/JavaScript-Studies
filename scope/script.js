e807e83f5e3a77539f900982c4ce876971a5d4e9
BaRobles <119386284+BaRobles@users.noreply.github.com>
2024-02-16 16:43:24 -0300

    studies in hoisting in JS

--- scope/script.js
+++ scope/script.js
@@ -25,4 +25,53 @@ function calcAge(birthYear){
 const firstName = 'Jonas';
 
 calcAge(1991);
-console.log(age);
+// console.log(age);
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Ba';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// the two below won't work, because both of them are a const variable and are in the TDZ
+// console.log(addExpr(2, 3));
+// console.log(addArrow); // this will log undefined on console
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b){
+  return a + b;
+}
+
+const addExpr = function(a , b){
+  return a + b;
+}
+
+//using var won't help, because varalways set to undefined by default
+// const addArrow = (a, b) => a + b;
+var addArrow = (a, b) => a + b;
+
+// Example of why we shouldn't use var
+if (!numProducts) deleteShoppingCart();
+// the condition above will be true, and the code above will delete all products, even though there were actually products on numProducts...
+
+
+var numProducts = 10;
+
+function deleteShoppingCart(){
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
+
+// variables declared with var will create a property on the global window object, which can create several bad consequences...
